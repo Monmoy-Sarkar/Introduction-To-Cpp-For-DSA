8f7008ff62a243fbdda9111f8e52ad7ff1a30248
Monmoy-Sarkar <monmoysarkar12345@gmail.com>
2026-09-22 22:54:41 +0600

    print

---
+++ m.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main(){
+    // normal print
+    std::cout << "ISHFAk\n";
+
+    int x =10;
+    char c = 'A';
+    double d =12.22;
+
+    // variable print
+    std::cout << x << " " << c << std::endl << d << std::endl;
+    std::cout << "My favorite number is: " << x;
+
+
+
+
+
+    // skks
+
+    // ddd
+    return 0;
+}
+
